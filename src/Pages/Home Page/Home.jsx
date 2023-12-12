642d16f7286b48907acb42dc35eb78fa9b0381cf
Reuben <treuben90@yahoo.com>
2023-12-12 16:12:54 +0200

    created the styling for the mobile on the home page

--- src/Pages/Home Page/Home.jsx
+++ src/Pages/Home Page/Home.jsx
@@ -36,29 +36,7 @@ function Home({ data }) {
   return (
     <div className="home-container">
       <NavBar />
-      {/* <div className="navBar">
-        <img src={logoAvatar} alt="karma Logo" className="imageLogo" />
 
-        <nav className={showNav ? "nav" : "inactive"}>
-          <ul className="nav__links">
-            <li className="links home">Home</li>
-            <li className="links service">Service</li>
-
-            <li className="links about">About</li>
-            <Link to="/contact">
-              <li className="links contact">Contact</li>
-            </Link>
-            <button className="link-btn" onClick={handleUnshowNav}>
-              &times;
-            </button>
-          </ul>
-        </nav>
-
-        <GiHamburgerMenu
-          className={showNav ? "inactive" : "burger"}
-          onClick={handleShowNav}
-        />
-      </div> */}
       <div className="carousel">
         <div className="mainPageTextAndButton">
           <div className="headersLetters">
@@ -112,9 +90,9 @@ function Home({ data }) {
         </div>
       </div>
       <div className="cards">
-        {/* <Cards />
         <Cards />
-        <Cards /> */}
+        <Cards />
+        <Cards />
       </div>
       <div>
         <Footer />
